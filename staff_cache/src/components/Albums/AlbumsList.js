20342b055a5912e228f5ce3f2f70c75bb86a3412
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-08-04 02:18:49 +1200

    expandable panel for staff

--- staff_cache/src/components/Albums/AlbumsList.js
+++ staff_cache/src/components/Albums/AlbumsList.js
@@ -1,5 +1,36 @@
 import React from 'react'
+import { useFetchAlbumsQuery } from '../../store'
+import { toast } from 'react-toastify'
+import Skeleton from '../Skeleton'
+import ExpandablePanel from '../ExpandablePanel'
+import Button from '../Button'
 
 export default function AlbumsList({ staff }) {
-  return <div>Albums for {staff.name}</div>
+  const { data, error, isLoading } = useFetchAlbumsQuery(staff)
+
+  let content
+
+  if (isLoading) {
+    content = <Skeleton custom='w-full h-10' times={6} />
+  } else if (error) {
+    content = toast.error(error.message)
+  } else {
+    content = data.map((album) => {
+      const header = <div>{album.title}</div>
+
+      return (
+        <ExpandablePanel key={album.id} header={header}>
+          Random Photos
+        </ExpandablePanel>
+      )
+    })
+  }
+
+  console.log(data, error, isLoading)
+  return (
+    <div className=''>
+      <div>Albums for {staff.name}</div>
+      <div className=''>{content}</div>
+    </div>
+  )
 }
